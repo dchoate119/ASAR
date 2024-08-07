f43448ddc14ef5cd3237ca32cd1acf8abc2fa1fa
dchoate119 <dchoate119@gmail.com>
2024-08-07 16:19:38 -0400

    autosave troubleshooting and continued intrinsic/extrinsic matrix research

---
+++ s1/Msc/autoreloadtest.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pykitti
+# from vedo import *
+# from ipyvtklink.viewer import ViewInteractiveWidget
+
+%load_ext autoreload
+%autoreload 2
+%autosave 180
+# %matplotlib notebook
