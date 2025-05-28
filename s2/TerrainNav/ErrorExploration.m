5a714569d267aa26c8a2e8bbb2e93644c4efae94
Daniel Choate <141774203+dchoate119@users.noreply.github.com>
2025-05-28 14:51:42 -0400

    Add files via upload
    Updated linearization about midpoint, instead of endpoint

--- s2/TerrainNav/ErrorExploration.m
+++ s2/TerrainNav/ErrorExploration.m
@@ -10,11 +10,16 @@ syms s t p1 q1 p2 q2 d
 % j12 = -s*(sin(t)*p1 + cos(t)*q1);
 % j21 = sin(t)*p1 + cos(t)*q1;
 % j22 = s*(cos(t)*p1 - sin(t)*q1);
-% Note: wedge locations = 0 0 0 d
-j11 = 0;
-j12 = 0;
-j21 = 0;
-j22 = 0;
+% % Note: wedge locations = 0 0 d 0
+% j11 = 0;
+% j12 = 0;
+% j21 = 0;
+% j22 = 0;
+% Note: wedge locations = -d/2 0 d/2 0
+j11 = cos(t)*(-d/2);
+j12 = -s*(sin(t)*(-d/2));
+j21 = sin(t)*(-d/2);
+j22 = s*(cos(t)*(-d/2));
 
 % Wedge 2
 % Note: wedge locations = p1 q1 p2 q2
@@ -22,11 +27,16 @@ j22 = 0;
 % j32 = -s*(sin(t)*p2 + cos(t)*q2);
 % j41 = sin(t)*p2 + cos(t)*q2;
 % j42 = s*(cos(t)*p2 - sin(t)*q2);
-% Note: wedge locations = 0 0 0 d
-j31 = cos(t)*d;
-j32 = -s*sin(t)*d;
-j41 = sin(t)*d;
-j42 = s*cos(t)*d;
+% % Note: wedge locations = 0 0 d 0
+% j31 = cos(t)*d;
+% j32 = -s*sin(t)*d;
+% j41 = sin(t)*d;
+% j42 = s*cos(t)*d;
+% Note: wedge locations = -d/2 0 d/2 0 
+j31 = cos(t)*(d/2);
+j32 = -s*sin(t)*(d/2);
+j41 = sin(t)*(d/2);
+j42 = s*cos(t)*(d/2);
 
 % Constants
 j13 = 1;
